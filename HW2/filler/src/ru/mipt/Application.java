46016e023b4b5964b18e172e4480967e5b76c310
asharykin <aleksandr-sharykin@mail.ru>
2025-11-02 23:05:17 +0300

    Added filler service from HW2

---
+++ HW2/filler/src/ru/mipt/Application.java
@@ -0,0 +1,34 @@
+package ru.mipt;
+
+import ru.mipt.model.User;
+import ru.mipt.service.CsvReaderService;
+import ru.mipt.service.DbInitService;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class Application {
+
+    private static final String DB_URL;
+    private static final String DB_USER;
+    private static final String DB_PASSWORD;
+
+    static {
+        DB_URL = String.format("jdbc:mariadb://%s:%s/%s",
+                System.getenv("DB_HOST"), System.getenv("DB_PORT"), System.getenv("DB_DATABASE"));
+        DB_USER = System.getenv("DB_USER");
+        DB_PASSWORD = System.getenv("DB_PASSWORD");
+    }
+
+    public static void main(String[] args) {
+        CsvReaderService csvReaderService = new CsvReaderService();
+        List<User> users = csvReaderService.readUsersFromCsvFile();
+
+        try (DbInitService dbInitService = new DbInitService(DB_URL, DB_USER, DB_PASSWORD)) {
+            dbInitService.createTableIfNotExists();
+            dbInitService.insertUsersIntoTable(users);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ HW2/filler/src/ru/mipt/service/CsvReaderService.java
@@ -0,0 +1,37 @@
+package ru.mipt.service;
+
+import ru.mipt.model.User;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvReaderService {
+
+    private final String separator = ",";
+    private final String filePath = "data.csv";
+
+    public List<User> readUsersFromCsvFile() {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            br.readLine(); // пропускаем первую строку с заголовком
+            String line;
+            while ((line = br.readLine()) != null) {
+                User user = parseLineIntoUser(line);
+                users.add(user);
+            }
+            return users;
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private User parseLineIntoUser(String line) {
+        String[] data = line.split(separator);
+        String name = data[0].trim();
+        int age = Integer.parseInt(data[1].trim());
+        return new User(name, age);
+    }
+}
